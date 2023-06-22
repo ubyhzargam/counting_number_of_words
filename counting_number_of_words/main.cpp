0daf962dd7668a0a495278c1b2443cb346878c1a
ubyhzargam <ubyhzargam@gmail.com>
2023-06-22 16:36:22 +0530

    Initial Commit

---
+++ counting_number_of_words/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  counting_number_of_words
+//
+//  Created by Uby H on 22/06/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
